6ce8aa009d26c80591737102ba0dc0c4e717c5dc
lauttigl <ljiglesias@outlook.com>
2023-03-21 17:46:25 -0300

    trae los productos del json

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import fs from "fs"
 
 const router = Router()
 
-router.get("/",(req,res) => {
-    res.render("test", {})
-})
-//verirficar si va el {}
+// router.get("/",(req,res) => {
+//     res.render("home", {})
+// })
+router.get("/", (req, res) => {
+    fs.readFile("products.json", (err, data) => {
+      if (err) throw err;
+      const products = JSON.parse(data);
+      res.render("home", { products });
+    });
+  });
+
 
 export default router
